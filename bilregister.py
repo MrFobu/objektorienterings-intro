edcc6ab8ac7d7b9d703faa696832ae1c9abcd731
Andreas Jansson <Mr_Fobu@hotmail.com>
2021-10-27 11:04:31 +0200

    le progress

--- bilregister.py
+++ bilregister.py
@@ -1,30 +1,23 @@
 class Car():
-    '''
-    En klass som håller reda på några egenskaper hos en bil.
-    '''
-    # Metoden __init__, körs alltid då ett objekt skapas
-
     def __init__(self, brand, color, mileage):
-        # Nedanstående variabler kallas för attribut.
-        # Alla objekt av klassen Car har egna värden på dessa.
         self.brand = brand
         self.color = color
         self.mileage = mileage
 
     def get_brand(self):
-        '''
-        Skriver ut bilmärket
-        '''
         print(self.brand)
+    
+    def get_mileage(self):
+        return self.mileage
 
     def set_brand(self, new_brand):
-        '''
-        Parameter: new_brand | sträng
-        Uppdaterar bilmärket om det existerar. Om det inte existerar
-        så tilldelas aktuellt objekt märket enligt parametern.
-        '''
         self.brand = new_brand
 
+    def set_color(self, new_color):
+        self.color = new_color
+
+    def set_milage(self, new_mileage):
+        self.milage = new_mileage
 
 # ----------Huvudprogram----------
 # Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
@@ -33,3 +26,6 @@ a_car = Car('Volvo', 'Blå', 1587)
 a_car.get_brand()
 a_car.set_brand('Renault')
 a_car.get_brand()
+
+b_car = Car("Royce", "white", 1200)
+b_car.get_brand()
